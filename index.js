ce1ee286c1daba9e18966d98f0e1809cd8cc70db
Akhilchennu <36916210+Akhilchennu@users.noreply.github.com>
2020-04-28 10:59:29 +0530

    updated solved asynchronous issue

--- index.js
+++ index.js
@@ -29,43 +29,50 @@ const app_id = '12dd05c446f6061e6c1e69b3022a3902'
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// async function getData(weatherData){
-//     let responseData=[];
-//      weatherData.forEach((city) => {
-//         const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${app_id}`
-//         fetch(url).then(res => res.json())
-//             .then(body => {
-//                 responseData.push(body);
-//                 // if (responseData.length === weatherData.length) {
-//                 //     res.send({ success: true, weather: responseData });
-//                 // }
-//             }).catch((error) => { res.send({ success: false }) });
-//     });
-//     return  responseData;
-// }
+const fetchData=async (city)=>{
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${app_id}`
+    // const data= await fetch(url);
+    // return await data;   
+    // const dataValue=await data.then( res => { return res.json()});  
+    // return data;
+    const data=await fetch(url).then( res => { return res.json()});
+    return data;
+}
 
-app.post('/getWeatherUpdates', (req, res) => {
+app.post('/getWeatherUpdate',async (req,res)=>{
     const weatherData = req.body.weatherData;
-    if (Array.isArray(weatherData) && weatherData.length > 0) {
-        let responseData = [];
-        weatherData.forEach((city) => {
-            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${app_id}`
-            fetch(url).then(res => res.json())
-                .then(body => {
-                    responseData.push(body);
-                    if (responseData.length === weatherData.length) {
-                        res.send({ success: true, weather: responseData });
-                    }
-                }).catch((error) => { res.send({ success: false }) });
-        });
-        //  res.send({ success: true, weather: getData(weatherData) });
-
-    } else {
-        res.send({ success: false });
-    }
-
+    const promiseValue=weatherData.map(async (data)=>{
+        const cityData=await fetchData(data);
+        return cityData; 
+    })
+   const responseData=await Promise.all(promiseValue);
+    res.send({ success: true, weather: responseData })
 })
 
+
+// app.post('/getWeatherUpdates',async (req, res) => {
+//     const weatherData = req.body.weatherData;
+//     debugger
+//     if (Array.isArray(weatherData) && weatherData.length > 0) {
+//         let responseData = [];
+//         await weatherData.forEach(async (city) => {
+//             const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${app_id}`
+//              fetch(url).then(res => res.json())
+//                 .then(body => {
+//                     responseData.push(body);
+//                     if (responseData.length === weatherData.length) {
+//                         res.send({ success: true, weather: responseData });
+//                     }
+//                 }).catch((error) => { res.send({ success: false }) });
+//         });
+//         //  res.send({ success: true, weather: getData(weatherData) });
+
+//     } else {
+//         res.send({ success: false });
+//     }
+
+// })
+
 app.listen(3002, () => {
-    console.log('server listening at 3002');
+    console.log('server listening at 3003');
 })
